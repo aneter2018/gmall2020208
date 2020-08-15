7dc06ea779f45dd5b8a5d1fee7a6202737fa2297
aneter2018 <503316976@qq.com>
2020-08-15 16:12:49 +0800

    '修改上传github'

---
+++ gmall-api/src/main/java/com/aneter/gmall/service/UserService.java
@@ -0,0 +1,10 @@
+package com.aneter.gmall.service;
+
+
+import com.aneter.gmall.bean.UserInfo;
+
+import java.util.List;
+
+public interface UserService {
+    List<UserInfo> userList();
+}
---
+++ gmall-api/src/main/java/com/aneter/gmall/service/impl/UserServiceImpl.java
@@ -0,0 +1,26 @@
+package com.aneter.gmall.service.impl;
+
+import com.aneter.gmall.user.bean.UserInfo;
+import com.aneter.gmall.user.mapper.UserInfoMapper;
+import com.aneter.gmall.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserInfoMapper userInfoMapper;
+
+    @Override
+    public List<UserInfo> userList(){
+
+        List<UserInfo> userInfos = userInfoMapper.selectAllUserAndAddress();
+
+        return userInfos;
+    }
+
+}
