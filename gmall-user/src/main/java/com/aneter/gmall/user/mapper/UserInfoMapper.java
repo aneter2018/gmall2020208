4d81a084ea6e15abcb856ba51b5209d8599f091c
aneter2018 <503316976@qq.com>
2020-08-15 09:52:11 +0800

    2020.8.15配置了数据源,小测试

---
+++ gmall-user/src/main/java/com/aneter/gmall/user/mapper/UserInfoMapper.java
@@ -0,0 +1,7 @@
+package com.aneter.gmall.user.mapper;
+
+import com.aneter.gmall.user.bean.UserInfo;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserInfoMapper extends Mapper<UserInfo> {
+}
---
+++ gmall-user/src/main/java/com/aneter/gmall/user/service/UserService.java
@@ -0,0 +1,9 @@
+package com.aneter.gmall.user.service;
+
+import com.aneter.gmall.user.bean.UserInfo;
+
+import java.util.List;
+
+public interface UserService {
+    List<UserInfo> userList();
+}
